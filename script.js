59c53d5920ad769212ed8eef2d561efe407335ba
Jayesh Dambale <88830136+JAYESHDAMBALE@users.noreply.github.com>
2026-01-08 20:54:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+//Text Change
+document.getElementById("change_text").addEventListener("click",function() {
+	const newText  = ["Bye", "Good Bye", "To", "Your", "Class"];
+	const divs = document.getElementByClassName("divison");
+
+	for(let i = 0; i < divs.length; i++){
+		divs[i].innerText = newText[i]; 
+	}
+})
