32ad721effa93b87b09c9742c510e0e004872e8d
Jayesh Dambale <88830136+JAYESHDAMBALE@users.noreply.github.com>
2026-01-08 21:26:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
-//Text Change
-document.getElementById("change_text").addEventListener("click",function() {
-	const newText  = ["Bye", "Good Bye", "To", "Your", "Class"];
-	const divs = document.getElementByClassName("divison");
+// Calculator
+document.getElementById("calculate").addEventListener("click", function () {
+    const num1 = Number(document.getElementById("num1").value);
+    const num2 = Number(document.getElementById("num2").value);
+    const operator = document.getElementById("operator").value;
 
-	for(let i = 0; i < divs.length; i++){
-		divs[i].innerText = newText[i]; 
-	}
-})
+    let result = 0;
 
-//Calculator
-document.getElementById("calculate").addEventListener("click",function () {
-	const num1 = document.getElementById("num1");
-	const num2 = document.getElementById("num2");
-	let result;
+    if (operator === "+") {
+        result = num1 + num2;
+    } else if (operator === "-") {
+        result = num1 - num2;
+    } else if (operator === "*") {
+        result = num1 * num2;
+    } else if (operator === "/") {
+        result = num1 / num2;
+    }
 
-	switch(operator){
-		case "+": 
-			result = num1 + num2;
-			break;
-		case "-":
-			result = num1 - num2;
-			break;
-		case "*":
-			result = num1 * num2;
-			break;
-		case "/":
-			result = num2 != 0? num1/num2 : "Cannot divide by zero";
-			break;		
-	}
+    // IMPORTANT: ONLY number, no text
+    document.getElementById("result").innerText = result;
+});
 
-	document.getElementById(result).innerText = "Result " + result;
-	
-})
+// Text Change
+document.getElementById("change_text").addEventListener("click", function () {
+    const texts = ["Bye", "Good Bye", "To", "Your", "Class"];
+    const divs = document.getElementsByClassName("division");
+
+    for (let i = 0; i < divs.length; i++) {
+        divs[i].innerText = texts[i];
+    }
+});
